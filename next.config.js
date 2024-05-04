3cd2e4dae18a8058af07b3aa3f8fd87b01c700f1
amaury.pichat@gmail.com <amaury.pichat@gmail.com>
2024-05-04 22:31:49 +0200

    rr

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const withTM = require("next-transpile-modules")(["@kitware/vtk.js"]);
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
+  // distDir:'export',
+  // output: 'export',
   images: {
     unoptimized: true,
   },
